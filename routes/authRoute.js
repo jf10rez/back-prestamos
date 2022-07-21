7013ab5d470ab0ec25dffa68804872287fdd75b3
jf10rez <jflorezlondono@hotmail.com>
2022-07-21 14:36:11 -0500

    route of auth added

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -3,10 +3,24 @@
 // Host + /api/auth
 
 const { Router } = require('express')
+const passport = require('passport')
 const router = Router()
 
-const { loginUser } = require('../controllers/authController')
+router.use(passport.initialize())
+
+const { loginUser, loginWithFacebook } = require('../controllers/authController')
+require('../middlewares/facebook')
 
 router.post( '/', loginUser )
+router.get("/facebook", passport.authenticate("facebook"));
+
+router.get(
+  "/facebook/callback",
+  passport.authenticate("facebook", {
+    successRedirect: "/",
+    failureRedirect: "/fail"
+  }),
+  loginWithFacebook
+);
 
 module.exports = router
