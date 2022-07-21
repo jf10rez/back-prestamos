236006b004ff7bc58c2a2fcd65556ee630540969
jf10rez <jflorezlondono@hotmail.com>
2022-07-21 15:13:52 -0500

    change model user

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,37 +1,28 @@
 const { Schema, model } = require("mongoose");
 
 
-const PrestamoSchema = Schema({
-    amount: {
-        type: Number,
+const UserSchema = Schema({
+    email: {
+        type: String,
         required: true
     },
-    document: {
-        type: Number,
+    password: {
+        type: String,
         required: true
     },
     name: {
         type: String,
         required: true
     },
-    quota: {
-        type: Number,
-        required: true
-    },
-    state: {
-        type: Number,
-        required: true
-    },
-    startDate: {
-        type: String,
-        required: true
+    celPhone: {
+        type: Number
     }
 })
 
-PrestamoSchema.method('toJSON', function(){//Para devolver la info en el controlador con el objeto modificado
+UserSchema.method('toJSON', function(){//Para devolver la info en el controlador con el objeto modificado
     const { __v, _id, ...object } = this.toObject()
     object.id = _id
     return object
 })
 
-module.exports = model('Prestamo', PrestamoSchema)
+module.exports = model('User', UserSchema)
