36f17b3d50b5467bf585177b5f4dba2d781a6a8b
jf10rez <jflorezlondono@hotmail.com>
2022-07-21 21:56:34 -0500

    add validators and jwt

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,17 +2,41 @@
 
 // Host + /api/auth
 
-const { Router } = require('express')
-const passport = require('passport')
-const router = Router()
+const { Router } = require("express");
+const { check } = require("express-validator");
+const passport = require("passport");
+const router = Router();
+const { validateFields } = require("../middlewares/validate-fields");
 
-router.use(passport.initialize())
+router.use(passport.initialize());
 
-const { loginUser, loginWithFacebook, createUser } = require('../controllers/authController')
-require('../middlewares/facebook')
+const {
+  loginUser,
+  loginWithFacebook,
+  createUser,
+} = require("../controllers/authController");
+require("../middlewares/facebook");
 
-router.post( '/', loginUser )
-router.post( '/new', createUser )
+router.post(
+  "/",
+  [
+    check("email", "El email es obligatorio").not().isEmpty(),
+    check("password", "La contraseña es obligatoria").not().isEmpty(),
+    validateFields,
+  ],
+  loginUser
+);
+
+router.post(
+  "/new",
+  [
+    check("email", "El email es obligatorio").not().isEmpty(),
+    check("password", "La contraseña es obligatoria").not().isEmpty(),
+    check("name", "El nombre es obligatorio").not().isEmpty(),
+    check("pin", "El pin es obligatorio").not().isEmpty(),
+  ],
+  createUser
+);
 
 router.get("/facebook", passport.authenticate("facebook"));
 
@@ -20,9 +44,9 @@ router.get(
   "/facebook/callback",
   passport.authenticate("facebook", {
     successRedirect: "/",
-    failureRedirect: "/fail"
+    failureRedirect: "/fail",
   }),
   loginWithFacebook
 );
 
-module.exports = router
+module.exports = router;
