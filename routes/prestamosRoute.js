1d7832502d8c833444b7f39c72e8719a9d02992d
jf10rez <jflorezlondono@hotmail.com>
2022-07-21 22:18:05 -0500

    validators added

--- routes/prestamosRoute.js
+++ routes/prestamosRoute.js
@@ -3,11 +3,27 @@
 // Host + /api/prestamo
 
 const { Router } = require('express')
+const { check } = require('express-validator')
 const router = Router()
 
 const { getPrestamos, newPrestamo } = require('../controllers/prestamosController')
+const { isDate } = require('../helpers/isDate')
+const { validateFields } = require('../middlewares/validate-fields')
+const { validateJWT } = require('../middlewares/validate-jwt')
+
+router.use( validateJWT )
 
 router.get( '/', getPrestamos )
-router.post( '/', newPrestamo )
+router.post( '/', [
+    check("amount", "El valor a prestar es obligatorio").not().isEmpty(),
+    check("document", "El documento es obligatorio").not().isEmpty(),
+    check("name", "El nombre es obligatorio").not().isEmpty(),
+    check("quota", "La cantidad de cuotas son obligatorias").not().isEmpty(),
+    check("percentage", "El porcentaje a prestar es obligatorio").not().isEmpty(),
+    check("state", "El estado es obligatorio").not().isEmpty(),
+    check("startDate", "La fecha de inicio es obligatoria").custom( isDate ),
+    validateFields
+
+], newPrestamo )
 
 module.exports = router
